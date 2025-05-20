7ea63596c5ca29e61a2ded3dcac2012ba3cb7410
tplusk <macfelelhun@gmail.com>
2025-05-20 14:27:52 +0100

    added welcomePage routing to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+import NavBar from "./components/Navbar";
+import { WelcomePage } from "./components/welcomePage";
 
 function App() {
   return (
     <>
-      <h1>
-        <WelcomePage />
-      </h1>
+      <main>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<WelcomePage />}></Route>
+        </Routes>
+      </main>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,12 @@
 import { link } from "react-router";
+
+const NavBar = () => {
+  return (
+    <>
+      <span className="newsspan">
+        <Link to="/">Welcome | </Link>
+      </span>
+    </>
+  );
+};
+export default NavBar;
