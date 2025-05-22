c3c1151c71a2c4ac50f5bc09658eb07b219cc392
tplusk <macfelelhun@gmail.com>
2025-05-22 10:59:35 +0100

    amended Layout, image source

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,22 @@
 import { Outlet, Link } from "react-router";
+import image from "./images-logos/news-img.png";
 
 export const Layout = () => {
   return (
     <>
+      <div>
+        <img id="newsGlobe" src={image} alt="News Globe Logo" />
+      </div>
       <div>
         <br></br>
       </div>
       <nav>
         <menu>
           <li>
-            <Link to="/">Home Page </Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/articles">Articles </Link>
+            <Link to="/articles">Articles</Link>
           </li>
         </menu>
       </nav>
